c6cd63765b3324b6d36284ffd0bcc35cbb737ce5
kkmonlee <kkmonlee@gmail.com>
2016-11-19 10:04:40 +0000

    Comparison successfully works

--- app/src/main/java/com/example/aa/savethechildren/Comparison.java
+++ app/src/main/java/com/example/aa/savethechildren/Comparison.java
@@ -9,14 +9,19 @@ import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 
 public class Comparison extends AppCompatActivity {
 
-    public String E1 = "A";
+    public String E1;
+    public float EQ1;
     public String E2;
+    public float EQ2;
     public String P1;
+    public float PQ1;
     public String P2;
+    public float PQ2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,37 +30,32 @@ public class Comparison extends AppCompatActivity {
 
         // changes Title and Subtitle
         android.support.v7.app.ActionBar ab = getSupportActionBar();
-
         ab.setTitle("Compare your diets");
-       // compareEnergy();
-        //Find the three nutrients most lacking from the current diet
-        //Set the three textView to these
+
+        //These 2 functions pick the two options the user can pick for energy and protein
+        compareEnergy();
+        compareProtein();
+
+        //This makes the Energy and Protein headers
         TextView needed_nutrition1 = (TextView)findViewById(R.id.needed_nutrition1);
         needed_nutrition1.setText("Energy");
-
         TextView needed_nutrition2 = (TextView)findViewById(R.id.needed_nutrition2);
         needed_nutrition2.setText("Protein");
 
-        //Find the 3 best foods to tackle each and set the check box labels
-        //Checkboxes for nutrition 1
+        //This assigns the options decided earlier onto the check boxes.
         CheckBox nutrition1_1 = (CheckBox)findViewById(R.id.nutrition1_1);
-        String temp="ffasd";
         nutrition1_1.setText(E1);
-
-
         CheckBox nutrition1_2 = (CheckBox)findViewById(R.id.nutrition1_2);
         nutrition1_2.setText(E2);
-
-        //Checkboxes for nutrition 2
         CheckBox nutrition2_1 = (CheckBox)findViewById(R.id.nutrition2_1);
-        nutrition2_1.setText("nutrition2_1 checkbox");
+        nutrition2_1.setText(P1);
         CheckBox nutrition2_2 = (CheckBox)findViewById(R.id.nutrition2_2);
-        nutrition2_2.setText("nutrition2_2 checkbox");
+        nutrition2_2.setText(P2);
 
     }
 
+    //T
     Intent intent = getIntent();
-
     public void ToNewDiet(View view) {
         Intent intent = new Intent(this, NewDiet.class);
         Button toNewDiet = (Button) findViewById(R.id.toNewDiet);
@@ -71,20 +71,40 @@ public class Comparison extends AppCompatActivity {
         int i=0;
         for (String[] str : scoreList) {
             if (i<13) {
-                String[] fhab = str[i].split(",");
-                System.out.println(fhab[1]);
-                if ((Float.valueOf(fhab[5]) > 10.0)) {
+                if ((Float.valueOf(str[3]) > 10.0)) {
                     if (first == true) {
-                        E1 = fhab[0];
+                        E1 = str[0];
+                        EQ1=Float.valueOf(str[1]);
                         first = false;
-                    } else
-                        E2 = fhab[0];
+                    } else {
+                        E2 = str[0];
+                        EQ2 = Float.valueOf(str[1]);
+                    }
                 }
                 i++;
             }
         }
-
-
     }
-
+    public void compareProtein(){
+        boolean first=true;
+        InputStream inputStream = getResources().openRawResource(R.raw.fhab);
+        CsvFile csvFile = new CsvFile(inputStream);
+        List<String[]> scoreList = csvFile.read();
+        int i=0;
+        for (String[] str : scoreList) {
+            if (i<13) {
+                if ((Float.valueOf(str[1]) > Float.valueOf(MainActivity.plan[i][1])*52 ) & (Float.valueOf(str[1]) > 10.0)) {
+                    if (first == true) {
+                        P1 = str[0];
+                        PQ1=Float.valueOf(str[1]);
+                        first = false;
+                    } else {
+                        P2 = str[0];
+                        PQ2 = Float.valueOf(str[1]);
+                    }
+                }
+                i++;
+            }
+        }
+    }
 }
