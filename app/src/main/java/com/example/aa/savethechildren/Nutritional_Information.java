283eeffe3ad2ca096e9df123dd1224c9d3e37515
ManavGupta04 <manavgupta@hotmail.co.uk>
2016-11-19 00:10:45 +0000

    Test commit -
    button works from second to third

--- app/src/main/java/com/example/aa/savethechildren/Nutritional_Information.java
+++ app/src/main/java/com/example/aa/savethechildren/Nutritional_Information.java
@@ -3,6 +3,8 @@ package com.example.aa.savethechildren;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Nutritional_Information extends AppCompatActivity {
 
@@ -13,5 +15,12 @@ public class Nutritional_Information extends AppCompatActivity {
     }
 
     Intent intent = getIntent();
+
+    public void Compare_Diets(View view) {
+        Intent intent = new Intent(this, Comparison.class);
+        Button compare_Diets = (Button) findViewById(R.id.compare_Diets);
+        startActivity(intent);
+    }
+
 
 }
